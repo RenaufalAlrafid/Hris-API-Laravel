1834c125ab8d2420549b1e50066fcef2e9c13122
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2023-12-12 22:16:04 +0700

    create model and migration for users, divisi, and jabatan table

--- app/Models/jabatan.php
+++ app/Models/jabatan.php
@@ -5,25 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Model
+class jabatan extends Model
 {
     use HasFactory;
-    protected $table = "users";
+    protected $table = 'jabatans';
     protected $primaryKey = "id";
     protected $keyType = "int";
     public $incrementing = true;
     public $timestamps = true;
     protected $fillable = [
-        "username",
-        "password",
-        "email",
-        "token",
-        "jabatan_id",
-        "validation",
+        "name"
     ];
 
-    public function jabatan() : BelongsTo {
-        return $this->belongsTo(Jabatan::class, 'jabatan_id', "id");
+    public function divisi() : BelongsTo {
+        return $this->belongsTo(divisi::class, "divisi_id", "id");
+    }
+
+    public function users() : HasMany {
+        return $this->hasMany(User::class, "jabatan_id", "id");
     }
 }
---
+++ database/migrations/2023_12_12_145229_create_jabatans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jabatans', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->nullable(false)->unique('jabatans_name_unique');
+            $table->boolean("atasan")->nullable(false);
+            $table->boolean('validator')->nullable(false);
+            $table->unsignedBigInteger('divisi_id')->nullable(false);
+            $table->timestamps();
+            // Relasi
+            $table->foreign('divisi_id')->references('id')->on('divisis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jabatans');
+    }
+};
